6b85a6b6bf9af77432b7fed0496f413799dfd80c
Swapnil Sonar <swap.m.sonar@gmail.com>
2014-10-31 12:08:25 +0530

    Statewise representation of linkedin connections. But Soln dont work for all senarios, need to apply another way to resolve this.

--- js/geochart.js
+++ js/geochart.js
@@ -66,23 +66,140 @@ function drawMap() {
 				var addComponent = getProvienceCodeOfLocation(address);
 				// console.log("Address : " + address + " PC : " + addComponent);
 
-				if (addComponent != 'undefined') {
+				if (addComponent != selectedRegion && arr_province.indexOf(addComponent) == -1) {
 
-					if (arr_province.indexOf(addComponent) == -1) {
+					if ( typeof addComponent === "undefined") {
+						// NOP
+					} else {
+						console.log("Prov Address : " + address + " PC : " + addComponent);
 						arr_province.push(addComponent);
+					}
+
+				} else {
+					//NOP
+				}
+
+			}
+
+			// creating seperate array for storing cities names which belogs to particular province and state.
+			var arr_province_cities = [];
+
+			for (var i = 0; i < arr_province.length; i++) {
+
+				var state_array = [];
+
+				for (var k = 0; k < cities.length; k++) {
+
+					var mFullCountryName = getCountryName(selectedRegion);
+					var address = cities[k] + "," + mFullCountryName;
+					var addComponent = getProvienceCodeOfLocation(address);
+
+					// check whether city name is not COUNTRY NAME
+					if (address != getCountryName(selectedRegion)) {
+
+						if (addComponent == arr_province[i]) {
+							state_array.push(cities[k]);
+						}
+
+					};
+
+					/*if (addComponent != 'undefined' && addComponent != selectedRegion) {
+
+					 if (arr_province.indexOf(addComponent) == -1) {
+					 arr_province.push(addComponent);
+					 } else {
+					 //NOP
+					 }
+
+					 };*/
+
+					if (addComponent != selectedRegion && arr_province.indexOf(addComponent) == -1) {
+
+						if ( typeof addComponent === "undefined") {
+							// NOP
+						} else {
+							console.log("Prov Address : " + address + " PC : " + addComponent);
+							arr_province.push(addComponent);
+						}
 
 					} else {
 						//NOP
 					}
 
-				};
+				}
+				arr_province_cities.push(state_array);
+			}
+
+			for (var j = 0; j < arr_province.length; ++j) {
+				for (var i = 0; i < cities.length; ++i) {
+					if (j == i) {
+						arr_province_cities[arr_province[j]] = arr_province_cities[i];
+						delete arr_province_cities[i];
+					}
+				}
 
 			}
+
+			// for (var i=0; i < arr_province_cities["MH"].length; i++) {
+			// console.log(arr_province_cities["MH"][i]);
+			// };
+
+			var arr_statewise_connections = [];
+
+			for (var index = 0; index < arr_province.length; index++) {
+
+				// retrieving all cities from state/province
+				var arr_cities = [];
+				arr_cities = arr_province_cities[arr_province[index]];
+				console.log(arr_province[index] + " Cities : " + arr_cities.length);
+
+				// simple bucket for storing connections
+				var temp_state_connections = [];
+
+				for (var i = 0; i < arr_cities.length; i++) {
+
+					// get connections form particular city
+					var temp = getCitywiseConnctions(arr_cities[i], global_total_countrywise_connections[selectedRegion.toLowerCase()]);
+					console.log("City : " + arr_cities[i] + " Connections : " + temp.length);
+
+					for (var j = 0; j < temp.length; j++) {
+						temp_state_connections.push(temp[j]);
+					};
+
+				};
+
+				// [0: [Swapnil, Ashwini, Rahul..]]
+				if (temp_state_connections.length > 0)
+					arr_statewise_connections.push(temp_state_connections);
+
+			}
+
+			// console.log(arr_statewise_connections['LA'].length);
+
+			// We have this [0: [Swapnil, Ashwini, Rahul..]] and need to be this ["MH": [Swapnil, Ashwini, Rahul..]]
+			// so rename it.. :-)
+			for (var j = 0; j < arr_province.length; ++j) {
+				for (var i = 0; i < arr_statewise_connections.length; ++i) {
+
+					if (j == i) {
+						arr_statewise_connections[arr_province[j]] = arr_statewise_connections[i];
+						delete arr_statewise_connections[i];
+					}
+				}
+
+			}
+
+			console.log(arr_statewise_connections);
+
+			//End Code for make array of states accoding to the cities
 
 			for (var i = 0; i < arr_province.length; i++) {
 
 				var stateProvienceCode = selectedRegion + "-" + arr_province[i];
-				var tableDataRow = [stateProvienceCode, 10 * i];
+
+				console.log(stateProvienceCode);
+
+				var tableDataRow = [stateProvienceCode, arr_statewise_connections[arr_province[i]].length];
 				tableStateColumn.push(tableDataRow);
 
 			};
